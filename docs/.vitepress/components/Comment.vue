6c43239d1527ddabc26ee334f9f7577c8cc681ef
Komorebi <110453675+KomoriDev@users.noreply.github.com>
2023-08-24 14:01:33 +0800

    ✨ 增加论坛功能 (#20)

---
+++ docs/.vitepress/components/Comment.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+const { title } = useData()
+</script>
+
+<template>
+  <div class="comments">
+    <component
+      :is="'script'"
+      :key="title"
+      src="https://giscus.app/client.js"
+      data-repo="KomoriDev/NoneBotX"
+      data-repo-id="R_kgDOKGjPMA"
+      data-category="General"
+      data-category-id="DIC_kwDOKGjPMM4CYy2X"
+      data-mapping="title"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="top"
+      data-lang="zh-CN"
+      data-theme="preferred_color_scheme"
+      data-loading="lazy"
+      crossorigin="anonymous"
+    />
+  </div>
+</template>
